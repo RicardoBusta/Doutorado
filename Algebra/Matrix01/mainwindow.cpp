f9c95086bb17dcb37b1dd1a5583e8164e69abdd8
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2016-06-30 12:54:07 -0300

    Movendo arquivos e renomeando abas.

--- Algebra/Matrix01/mainwindow.cpp
+++ Algebra/Matrix01/mainwindow.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "block_tab.h"
-#include "cholesky_tab.h"
-#include "gauss_tab.h"
-#include "lu_tab.h"
-#include "performance_tab.h"
+#include "T1/block_tab.h"
+#include "T1/cholesky_tab.h"
+#include "T1/gauss_tab.h"
+#include "T1/lu_tab.h"
+#include "T1/performance_tab.h"
 
 #include <QMessageBox>
 
@@ -13,11 +13,15 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
                                           ui(new Ui::MainWindow) {
   ui->setupUi(this);
 
-  ui->tabWidget->addTab(new PerformanceTab(this), QString("Desempenho"));
-  ui->tabWidget->addTab(new BlockTab(this), QString("Bloco"));
-  ui->tabWidget->addTab(new GaussTab(this), QString("Gauss"));
-  ui->tabWidget->addTab(new LUTab(this), QString("LU"));
-  ui->tabWidget->addTab(new CholeskyTab(this), QString("Cholesky"));
+  // T1 Tabs
+  ui->tabWidget->addTab(new PerformanceTab(this), QString("T1 Desempenho"));
+  ui->tabWidget->addTab(new BlockTab(this), QString("T1 Bloco"));
+  ui->tabWidget->addTab(new GaussTab(this), QString("T1 Gauss"));
+  ui->tabWidget->addTab(new LUTab(this), QString("T1 LU"));
+  ui->tabWidget->addTab(new CholeskyTab(this), QString("T1 Cholesky"));
+
+  // T2 Tabs
+  //ui->tabWidget->addTab()
 
   showMaximized();
 }
